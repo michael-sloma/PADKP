3d63fa381ba0cbcfa85936280f52d7698a4d9a8f
michael sloma <mmierzejewski2@gmail.com>
2019-07-30 09:41:11 -0400

    Fix timestamp parsing

--- parse.py
+++ parse.py
@@ -100,7 +100,8 @@ class LogLine:
         self.contents = text[27:]
 
     def timestamp(self):
-        return dt.datetime.strptime(self.time_str, '[%a %b %d %H:%M:%S %Y]')
+        return self.time_str
+        #return dt.datetime.strptime(self.time_str, '[%a %b %d %H:%M:%S %Y]')
 
 
 def pre_filter(raw_line):
