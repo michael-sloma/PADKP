8d828dc0d11c7c450bb3f46d13e74a792019a633
Michael Sloma <msloma@xometry.com>
2019-11-17 14:40:10 -0500

    more website interaction

--- api_client.py
+++ api_client.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import requests
 import os
 
@@ -30,3 +31,14 @@ def award_dkp_from_dump(filepath, reason, value, counts_for_attendance, notes, t
                 "notes": notes
                }
     return requests.post('http://{}/api/upload_dump/'.format(API_ROOT), json=request, headers=get_headers(token))
+
+
+def create_character(name, character_class, status, token):
+    request = {'name': name, 'status': status, 'character_class': character_class}
+    return requests.post('http://{}/api/characters/'.format(API_ROOT), json=request, headers=get_headers(token))
+
+
+def award_dkp(character, value, attendance_value, notes, token):
+    time = dt.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    request = {'character': character, 'value': value, 'attendance_value': attendance_value, 'time': time, 'notes': notes}
+    return requests.post('http://{}/api/awards/'.format(API_ROOT), json=request, headers=get_headers(token))
