bd0b0a320468bf0ae3fdae8e0d78d12c2da2df98
Michael Sloma <msloma@xometry.com>
2020-01-25 12:11:36 -0500

    timezone handling

--- tests/test_timestamps.py
+++ tests/test_timestamps.py
@@ -3,7 +3,7 @@ import timestamps
 
 
 def test_time_from_gui_display():
-    test_s = "Jan 1, 10:00 AM"
+    test_s = "Jan 01, 10:00 AM"
     time = timestamps.time_from_gui_display(test_s)
     assert time.month == 1
     assert time.day == 1
@@ -11,7 +11,7 @@ def test_time_from_gui_display():
 
 
 def test_time_from_gui_display_pm():
-    test_s = "Jan 1, 10:00 PM"
+    test_s = "Jan 01, 10:00 PM"
     time = timestamps.time_from_gui_display(test_s)
     assert time.month == 1
     assert time.day == 1
@@ -21,17 +21,17 @@ def test_time_from_gui_display_pm():
 def test_time_to_gui_display():
     test_time = dt.datetime(month=1, day=1, year=2020, hour=10, minute=0)
     time_s = timestamps.time_to_gui_display(test_time)
-    assert time_s == "Jan 1, 10:00 AM"
+    assert time_s == "Jan 01, 10:00 AM"
 
 
 def test_time_to_gui_display_pm():
     test_time = dt.datetime(month=1, day=1, year=2020, hour=20, minute=0)
     time_s = timestamps.time_to_gui_display(test_time)
-    assert time_s == "Jan 1, 10:00 PM"
+    assert time_s == "Jan 01, 10:00 PM"
 
 
 def test_time_to_gui_display_pm():
-    test_s = "Jan 1, 10:00 PM"
+    test_s = "Jan 01, 10:00 PM"
     time = timestamps.time_from_gui_display(test_s)
     assert time.month == 1
     assert time.day == 1
@@ -40,5 +40,5 @@ def test_time_to_gui_display_pm():
 
 def test_timestamp_reversible():
     """ we can convert a string to datetime and back and it will be unchanged """
-    test_s = "Jan 1, 10:00 AM"
+    test_s = "Jan 01, 10:00 AM"
     assert timestamps.time_to_gui_display((timestamps.time_from_gui_display(test_s))) == test_s
