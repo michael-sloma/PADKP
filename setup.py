efa26f086e07edd5a144665b3320df0a64315664
Robert Uhl <rcuhljr@gmail.com>
2021-09-09 20:58:49 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=[{'script': "gui.py", "dest_base": "PADKP-2.1.1"}])
+setup(windows=[{'script': "gui.py", "dest_base": "PADKP-2.2.0"}])
