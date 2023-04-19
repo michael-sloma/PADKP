1d54159bebaaa42267691b938706e720c02e9c54
Robert Uhl <rcuhljr@gmail.com>
2023-04-18 20:32:31 -0400

    py2exe updates

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from distutils.core import setup
+from setuptools import find_packages
 import py2exe
 
-setup(windows=[{'script': "gui.py", "dest_base": "PADKP-2.5.0"}])
+setup( packages=find_packages(
+        where='',
+        include=['gui'],  # alternatively: `exclude=['additional*']`
+    ), windows=[{'script': "gui.py", "dest_base": "PADKP-2.6.0"}])
